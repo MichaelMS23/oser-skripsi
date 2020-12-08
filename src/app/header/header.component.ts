91c5b1e76aa3a1f2dca89b67d89664704c863fcf
Ronaldo Tantra <ronaldotantra@gmail.com>
2020-12-08 23:39:43 +0700

    add feature login

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,23 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let t = this.getCookie("loginToken")
+    $(".navbar-end").css("display","none")
+  }
+
+  getCookie(cname:any) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
   }
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-login',
@@ -14,6 +15,31 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  submitLogin() {  }
+  async submitLogin() { 
+    let response = await axios.create().request({
+      method: 'post',
+      url: 'http://3.208.28.174:3003/user/login',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      data: {   
+        userid: this.username,
+        password: this.password
+      },
+    });
+
+    if(response.status!=200){
+      this.username = ""
+      this.password = ""
+      return
+    }
+    
+    let date = new Date()
+    date.setTime(date.getTime() + 1 * 24 * 60 * 60 * 1000)
+    let expires = `expires=${date.toUTCString()}`;
+
+    document.cookie = `loginToken=${response.data.message[0].token}; ${expires}`
+    window.location.href = "/"
+  }
 
 }
